f489528a507370a74b06f355b65dd8f71bc6ce7a
Yara Alshakhs“git config --global user.email yshakhs@andrew.cmu.edu” <your@email.address>
2021-02-06 22:53:03 +0300

    regext_tester.rb update1

---
+++ regex_tester.rb
@@ -0,0 +1,10 @@
+class RegexTester
+
+    attr_accessor :statement, :pattern
+	def initialize (pattern="", statement)  
+		#  title, f and l are local variables
+		@pattern = pattern
+		@statement = statement
+
+	end 
+end
