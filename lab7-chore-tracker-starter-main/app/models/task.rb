a810e5ac358f3249478825e51d929fbe1928156c
Yara Alshakhs“git config --global user.email yshakhs@andrew.cmu.edu” <your@email.address>
2021-03-11 01:29:05 +0300

    second or third part 2 lab 7 commit

--- lab7-chore-tracker-starter-main/app/models/task.rb
+++ lab7-chore-tracker-starter-main/app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
     has_many :chores
 has_many :children, through: :chores
+
+validates_presence_of :name
+validates :points, presence: true, numericality: { greater_than: 0, less_than: 101 ,only_integer: true}
+
+
+scope :alphabetical, -> { order('name') }
+scope :active, -> { where(active: true) }
+
 end
